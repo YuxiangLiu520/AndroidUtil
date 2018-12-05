6e735cb635e2061e725a164999de110b59c0f2ea
liuyx <2479158033@qq.com>
2018-12-05 23:19:14 +0800

    first commit

---
+++ app/src/main/java/com/yxliu/androidutil/MainActivity.java
@@ -0,0 +1,26 @@
+package com.yxliu.androidutil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.yxliu.androidutil.util.PermissionPageUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_jump_permission_page).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PermissionPageUtils permissionPageUtils = new PermissionPageUtils(MainActivity.this);
+                permissionPageUtils.jumpPermissionPage();
+            }
+        });
+
+
+    }
+}
